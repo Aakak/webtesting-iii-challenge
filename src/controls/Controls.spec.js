557c32be17c205cc02d632dab23e9d3c59c40668
Akak Almazbekova <akak.almazbekova@gmail.com>
2019-10-14 16:07:47 -0700

    Added Dependencies and controls test

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,16 @@
 // Test away!
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Controls from "./Controls.js";
+
+
+
+test("<Controls />", () => {
+    it("locked", () => {
+      const { getByText, getByTestId, container } = render(
+        <Controls locked={true} closed={true} />
+ );
+      }
+}
+
+
